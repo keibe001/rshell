488c95d5bcd4fc7ef9f2320d63b37c471f6c489d
Kevin Eibeck <keibe001@ucr.edu>
2017-04-29 04:55:47 +0000

    first draft of Command.h

---
+++ Command.h
@@ -0,0 +1,27 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <string>
+
+class Command
+{
+    public:
+        Command();
+        virtual bool valid() = 0;
+        
+    private:
+        
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
