a27cb816b432f97c95652d60fb3221406b4c259c
Kevin Eibeck <keibe001@ucr.edu>
2017-04-30 20:21:14 +0000

    added connector functionality

--- Command.cpp
+++ Command.cpp
@@ -1,5 +1,5 @@
 
-
+using namespace std;
 
 #include "Command.h"
 
@@ -17,20 +17,27 @@ class normalCommand: public Command
         normalCommand(string s)
         {
             //use syscall to execute
+            //also "exit"
             //set validity (true if it executes properly, false otherwise)
         };
         bool valid()
         {
             return validity;
         };
+        int type();
+        {
+            return 1;
+        };
 };
 
-class andConnector: public Command
+class andConnector: public Command                          // &&
 {
     private:
         bool validity;
+        Command* l;
+        Command* r;
     public:
-        andConnector(Command* l, Command* r)
+        andConnector()      //Command* l, Command* r
         {
             
         };
@@ -38,14 +45,33 @@ class andConnector: public Command
         {
             return validity;
         };
+        int type();
+        {
+            return 2;
+        };
+        void set(Command* left, Command* right)
+        {
+            l = left;
+            r = right;
+            if(l->valid() && r->valid())
+            {
+                validity = true;
+            }
+            else
+            {
+                 validity = false;
+            }
+        }
 };
 
-class orConnector: public Command
+class orConnector: public Command                           // ||
 {
     private:
         bool validity;
+        Command* l;
+        Command* r;
     public:
-        orConnector(Command* l, Command* r)
+        orConnector()       //Command* l, Command* r
         {
             
         };
@@ -53,4 +79,74 @@ class orConnector: public Command
         {
             return validity;
         };
+        int type();
+        {
+            return 3;
+        };
+        void set(Command* left, Command* right)
+        {
+            l = left;
+            r = right;
+            if(!l->valid() && !r->valid())
+            {
+                 validity = false;
+            }
+            else
+            {
+                 validity = true;
+            }
+        }
 };
+
+
+class semiColonConnector: public Command                    // ;
+{
+    // private:
+    //     bool validity;
+    public:
+        semiColonConnector()
+        {
+            
+        };
+        bool valid()
+        {
+            return false;                                   // this is an error
+        };
+        int type();
+        {
+            return 4;
+        };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
